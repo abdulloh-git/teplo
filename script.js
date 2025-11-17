4d3fb87e3b426170f99be3da75b3c6b88458a957
smth <abdulloh.b2012@gmail.com>
2025-11-17 23:43:14 +0500

    second version

--- script.js
+++ script.js
@@ -1,77 +1,80 @@
-class Typing {
-    constructor(text) {
-        this.text = document.querySelector(text);
-        this.fullText = this.text.innerHTML;
-        this.text.innerHTML = "";
-        setTimeout(() => this.typeText(), 500);
-    }
-    typeText(x = 0) {
-        this.text.innerHTML += this.fullText[x];
-        x++;
-        if (x <= this.fullText.length) {
-            setTimeout(() => this.typeText(x), 300);
-        } else {
-            x = 0;
-            this.text.innerHTML = "";
-            setTimeout(() => this.typeText(x), 300);
-        }
-    }
-}
+// class Typing {
+//     constructor(text) {
+//         this.text = document.querySelector(text);
+//         this.fullText = this.text.innerHTML;
+//         this.text.innerHTML = "";
+//         setTimeout(() => this.typeText(), 500);
+//     }
+//     typeText(x = 0) {
+//         this.text.innerHTML += this.fullText[x];
+//         x++;
+//         if (x <= this.fullText.length) {
+//             setTimeout(() => this.typeText(x), 300);
+//         } else {
+//             x = 0;
+//             this.text.innerHTML = "";
+//             setTimeout(() => this.typeText(x), 300);
+//         }
+//     }
+// }
 
-const typingElement = new Typing(".header__nav-logo_desc");
 
-class Typing2 {
-    constructor(text) {
-        this.text = document.querySelector(text);
-        this.fullText = this.text.innerHTML;
-        this.text.innerHTML = "";
-        setTimeout(() => this.typeText2(), 500);
-    }
-    typeText2(x = 0) {
-        this.text.innerHTML += this.fullText[x];
-        x++;
-        if (x <= this.fullText.length) {
-            setTimeout(() => this.typeText2(x), 300);
-        } else {
-            x = 0;
-            this.text.innerHTML = "";
-            setTimeout(() => this.typeText2(x), 300);
-        }
-    }
-}
+// const typingElement = new Typing(".header__nav-logo_desc");
 
-const typingElement2 = new Typing2(".aboutUs__h1");
+// class Typing2 {
+//     constructor(text) {
+//         this.text = document.querySelector(text);
+//         this.fullText = this.text.innerHTML;
+//         this.text.innerHTML = "";
+//         setTimeout(() => this.typeText2(), 500);
+//     }
+//     typeText2(x = 0) {
+//         this.text.innerHTML += this.fullText[x];
+//         x++;
+//         if (x <= this.fullText.length) {
+//             setTimeout(() => this.typeText2(x), 300);
+//         } else {
+//             x = 0;
+//             this.text.innerHTML = "";
+//             setTimeout(() => this.typeText2(x), 300);
+//         }
+//     }
+// }
 
-class Typing3 {
-    constructor(text) {
-        this.text = document.querySelector(text);
-        this.fullText = this.text.innerHTML;
-        this.text.innerHTML = "";
-        setTimeout(() => this.typeText3(), 500);
-    }
-    typeText3(x = 0) {
-        this.text.innerHTML += this.fullText[x];
-        x++;
-        if (x <= this.fullText.length) {
-            setTimeout(() => this.typeText3(x), 300);
-        } else {
-            x = 0;
-            this.text.innerHTML = "";
-            setTimeout(() => this.typeText3(x), 300);
-        }
-    }
-}
+// const typingElement2 = new Typing2(".aboutUs__h1");
 
-const typingElement3 = new Typing3(".contacts__title");
+// class Typing3 {
+//     constructor(text) {
+//         this.text = document.querySelector(text);
+//         this.fullText = this.text.innerHTML;
+//         this.text.innerHTML = "";
+//         setTimeout(() => this.typeText3(), 500);
+//     }
+//     typeText3(x = 0) {
+//         this.text.innerHTML += this.fullText[x];
+//         x++;
+//         if (x <= this.fullText.length) {
+//             setTimeout(() => this.typeText3(x), 300);
+//         } else {
+//             x = 0;
+//             this.text.innerHTML = "";
+//             setTimeout(() => this.typeText3(x), 300);
+//         }
+//     }
+// }
+
+// const typingElement3 = new Typing3(".contacts__title");
 
 let captainNumber = document.querySelector(".captain__number");
 captainNumber.addEventListener("click", () => {
     alert("Мы не принемаем звонки. Контактируйте нас по телеграму, пожалуйста!")
 });
 
-let otherMembersNumber = document.querySelector(".otherMembers__number");
-otherMembersNumber.addEventListener("click", () => {
-    alert("Мы не принемаем звонки. Контактируйте нас по телеграму, пожалуйста!")
+let otherMembersNumber = document.querySelectorAll(".otherMembers__number");
+otherMembersNumber.forEach((element) => {
+    element.addEventListener("click", () => {
+        alert("Мы не принемаем звонки. Контактируйте нас по телеграму, пожалуйста!")
+    });
 });
 
 let button = document.querySelector(".footer__button-button");
